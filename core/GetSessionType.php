282bee13ec99a4f4f4c2c0f94cc92fd90d7e1a43
MAInvoker <mcinvokerm@gmail.com>
2018-05-11 22:05:42 -0500

    Primeros pasos en el index
    Obtener Tipo de sesión y administrar el menú según el tipo de usuario

--- core/GetSessionType.php
+++ core/GetSessionType.php
@@ -4,6 +4,8 @@ session_start();
 $_SESSION['type'] = 0; //soporte tecnico
 //$_SESSION['type'] = 1; //admin
 $type = $_SESSION['type'];
-return $type;
+$data = array("type" => $type);
+$response = array("response" => $data);
 
+echo json_encode($response);
 ?>
--- js/index.js
+++ js/index.js
@@ -5,12 +5,45 @@
 						objetos globales más usados
 
 #header_title: titulo del header cuando se carga un elemento en pantalla
-	Usage:
-		 $("#header_title").html('<i class="fa fa-history"></i> Crear Bitácora');
+	
+#support-menu: menu izquierdo del dashboard para soportes tecnicos
+
+#admin-menu: menu derecho de dashboard para admins
 
 /*****************************************************************************/
 
 $( document ).ready(function() {
     console.log( "ready!" );
-
+    getSessionType();
 });
+
+
+function getSessionType(){
+	$.ajax(
+      {
+        url: "core/GetSessionType.php",
+        type: "GET",
+        success: function(data){
+        	console.log(data);
+        	var json_response = JSON.parse(data);
+			if(json_response.response.type == 0){
+				
+				//mostrando menu
+				$("#support-menu").show();
+				getBitacoraForm();
+			}else if(json_response.response.type == 1){
+
+				//mostrando menu
+				$("#admin-menu").show();
+			}
+        },
+        error: function(data){
+          	
+        },
+    });
+}
+
+function getBitacoraForm(){
+	//ponendo el titulo
+	$("#header_title").html('<i class="fa fa-dashboard"></i> Crear Bitácora');
+}
